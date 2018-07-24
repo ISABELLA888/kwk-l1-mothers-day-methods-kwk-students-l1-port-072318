dadeb2d86a9453a13564e3d40fc5c4cc6698c233
ISABELLA888 <ipcastro888@gmail.com>
2018-07-24 18:20:54 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,10 +1,5 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-def mothers_day
-  mom = gets.strip 
-  puts "Happy Mother's day #{mom}"
-end
 
-mothers_day
 
 def mom_day(mom = "mother")
   puts "Happy Mother's day! #{mom}"
